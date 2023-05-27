a64e418ba57610b9cfd4950328338028fc27ea9c
alexandervianna <alexaraujo2209@gmail.com>
2023-05-27 00:51:11 -0300

    Subindo arquivos de deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/fontawesome.js', mode: 'client' }
+    { src: '~/plugins/fontawesome.js', mode: 'client' },
+    { src: '~/plugins/vercel-analytics', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
